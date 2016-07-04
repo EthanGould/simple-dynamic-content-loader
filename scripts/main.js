c91b65b45e8808970f9e3efc8297f6c38e2e4111
Ethan Gould <ethan.g.907@gmail.com>
2016-07-03 20:50:17 -0400

    built out front end, looking good

--- scripts/main.js
+++ scripts/main.js
@@ -16,7 +16,10 @@ module.updateContent = function( btn ) {
 	var msgId = $(btn).data('btn'); // get message ID
 	var msg = $('.js-content-msg[data-msg="' + msgId + '"]'); //find selected message
 	$('.js-content-msg').hide();
-	msg.show(); //show the message text
+	msg.fadeIn(); //show the message text
+
+	$('.js-content-btn').removeClass('active');
+	$(btn).addClass('active');
 };
 
 $(document).ready(function() {
